c9330e6e52e2834f6e6bfeec15d69d3f64f699d5
Max Branco <max_castelo_branco@hotmail.com>
2020-06-12 15:42:16 -0300

    Finished success page upon appointment creation

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
-import {Container} from './styles';
+import React, {useCallback, useMemo} from 'react';
+import Feather from 'react-native-vector-icons/Feather';
+import {useRoute, useNavigation} from '@react-navigation/native';
+import {format} from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+import {Container, Description, OkButton, OkButtonText, Title} from './styles';
+import responsivePixelSize from '../../utils/responsivePixelSize';
+
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-  return <Container />;
+  const {params} = useRoute();
+  const {reset} = useNavigation();
+  const {date} = params as RouteParams;
+
+  const formattedDate = useMemo(() => {
+    return format(date, "EEEE', dia' dd 'de' MMMM 'de' yyyy 'ás' HH:mm'h'", {
+      locale: ptBr,
+    });
+  }, [date]);
+
+  const handleEverythingOk = useCallback(() => {
+    reset({
+      routes: [{name: 'Dashboard'}],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Feather name="check" size={Number(responsivePixelSize(80))} color="#04d361" />
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
+      <OkButton onPress={handleEverythingOk}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
--- src/pages/AppointmentCreated/styles.ts
+++ src/pages/AppointmentCreated/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+import responsivePixelSize from '../../utils/responsivePixelSize';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 ${responsivePixelSize(24)}px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${responsivePixelSize(36)}px;
+  margin-top: ${responsivePixelSize(40)}px;
+  color: #f4ede8;
+  font-family: RobotoSlab-Medium;
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  font-size: ${responsivePixelSize(18)}px;
+  margin-top: ${responsivePixelSize(16)}px;
+  color: #999591;
+  font-family: RobotoSlab-Regular;
+  text-align: center;
+`;
+
+export const OkButton = styled(RectButton)`
+  background-color: #ff9000;
+  justify-content: center;
+  align-items: center;
+  border-radius: ${responsivePixelSize(12)}px;
+  margin-top: ${responsivePixelSize(24)}px;
+  padding: ${responsivePixelSize(12)}px ${responsivePixelSize(24)}px;
+`;
+
+export const OkButtonText = styled.Text`
+  font-size: ${responsivePixelSize(18)}px;
+  color: #312e38;
+  font-family: RobotoSlab-Medium;
+`;
